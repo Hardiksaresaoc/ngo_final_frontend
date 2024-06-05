6a6d4e17b1fa40dd7ba366f430ead66a3b5a09eb
hardik <hardiksaresa.oc@gmail.com>
2024-06-05 18:39:42 +0530

    update

--- src/app/admin/donationHistory/page.js
+++ src/app/admin/donationHistory/page.js
@@ -7,6 +7,8 @@ import styles from "./donationHistory.module.css";
 import Sidebar from "@/component/sidebar";
 import useAuth from "@/context/auth";
 import Loading from "@/app/loading";
+import { FaCircleCheck } from "react-icons/fa6";
+import { MdCancel, MdTimer } from "react-icons/md";
 export default function Page() {
   const user = useAuth(["ADMIN"]);
   const [data, setData] = useState([]);
@@ -131,6 +133,7 @@ export default function Page() {
       });
     }
   };
+  const renderField = (field) => (field ? field : "--");
 
   return !user && loading ? (
     <Loading />
@@ -224,42 +227,68 @@ export default function Page() {
             >
               <i className={`fa-solid fa-file-excel`}></i> Download Excel
             </button>
-            <table className={styles.table}>
-              <thead>
-                <tr>
-                  <th>Donation Id</th>
-                  <th>Donation Date</th>
-                  <th>Donor Details</th>
-                  <th>Fundraiser Details</th>
-                  <th>Amount</th>
-                  <th>Payment Option</th>
-                  <th>Payment Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data?.map((item) => (
-                  <tr key={item.donation_id_frontend}>
-                    <td>{item.donation_id_frontend}</td>
-                    <td>{formatDate(item.donation_date)} </td>
-                    <td>
-                      {item.donor_name}
-                      <br />
-                      {item.donor_email}
-                      <br />
-                      {item.donor_phone}
-                    </td>
-                    <td>
-                      {item.fundraiser?.firstName}
-                      <br />
-                      {item.fundraiser?.email}
-                    </td>
-                    <td>{item.amount}</td>
-                    <td>{item.payment_type}</td>
-                    <td>{item.payment_status}</td>
+            <div className={styles.tableMain}>
+              <table className={styles.table}>
+                <thead>
+                  <tr>
+                    <th>Donation Id</th>
+                    <th>Donation Date</th>
+                    <th>Donor Details</th>
+                    <th>Fundraiser Details</th>
+                    <th>Amount</th>
+                    <th>Payment Type</th>
+                    <th>Payment Status</th>
+                    <th>Donor PAN</th>
+                    <th>Donor Address</th>
+                    <th>Donor City</th>
+                    <th>Donor State</th>
+                    <th>Donor Country</th>
+                    <th>Donor Pincode</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {data?.map((item) => (
+                    <tr key={item.donation_id_frontend}>
+                      <td>{item.donation_id_frontend}</td>
+                      <td>{formatDate(item.donation_date)} </td>
+                      <td>
+                        {item.donor_name}
+                        <br />
+                        {item.donor_email}
+                        <br />
+                        {item.donor_phone}
+                      </td>
+                      <td>
+                        {item.fundraiser?.firstName}
+                        <br />
+                        {item.fundraiser?.email}
+                      </td>
+                      <td>{item.amount}</td>
+                      <td>{item.payment_type}</td>
+                      <td>
+                        {item.payment_status ? (
+                          item.payment_status == "success" ? (
+                            <FaCircleCheck color="#0FA900" />
+                          ) : item.payment_status == "failed" ? (
+                            <MdCancel color="red" />
+                          ) : (
+                            <MdTimer />
+                          )
+                        ) : (
+                          "--"
+                        )}
+                      </td>
+                      <td>{renderField(item.pan)}</td>
+                      <td>{renderField(item.donor_address)}</td>
+                      <td>{renderField(item.donor_city)}</td>
+                      <td>{renderField(item.donor_state)}</td>
+                      <td>{renderField(item.donor_country)}</td>
+                      <td>{renderField(item.donor_pincode)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         ) : (
           <Loading />
